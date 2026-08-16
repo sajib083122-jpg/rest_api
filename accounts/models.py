b6dc2fe937a05da97100b38f0178a96cde5c2bb3
sajib083122-jpg <sajib083122@gmail.com>
2026-08-16 22:31:31 +0600

    custom user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from accounts.managers import CustomUserManager
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    is_verified = models.BooleanField(default=False)    ##extra field to check if the user is verified or not
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,7 @@ REST_FRAMEWORK = {
 # SIMPLE_JWT CUSTOM settings যোগ করুন
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
@@ -214,3 +214,6 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'  # Specify the custom user model
